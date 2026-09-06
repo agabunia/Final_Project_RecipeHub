fa66e4b8d99c562eb7c9616a0d178aba5d6a56a1
AGabunia <gabuniasandro99@gmail.com>
2026-09-06 20:28:10 +0400

    update the secret key

--- app/config.py
+++ app/config.py
@@ -4,7 +4,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = 'secret-key'
+    SECRET_KEY = '82784b5a63535b34'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'instance', 'recipes.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SPOONACULAR_API_KEY = 'api-key'
